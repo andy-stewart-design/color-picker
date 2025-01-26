82c4bf367bdba31b705775875fe567615e0761b4
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2025-01-26 10:42:51 -0800

    Export dialog styles/copy functionality

--- src/components/ExportDialog/index.tsx
+++ src/components/ExportDialog/index.tsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import {
   Content,
   Description,
   Overlay,
   Portal,
   Title,
+  Close,
 } from "@radix-ui/react-dialog";
-import s from "./style.module.css";
 import { ColorDefinition } from "@/App";
 import { generateCSS } from "@/utils/generate-color-code-snippets";
+import s from "./style.module.css";
 
 interface Props {
   colors: ColorDefinition[];
@@ -21,11 +23,44 @@ export default function ExportDialog({ colors, names }: Props) {
     <Portal>
       <Overlay className={s.overlay}>
         <Content className={s.dialog}>
-          <Title>Export Colors</Title>
-          <Description></Description>
-          <pre dangerouslySetInnerHTML={{ __html: css }}></pre>
+          <div className={s.header}>
+            <Title>Export palette</Title>
+            <Description></Description>
+            <Close>
+              <CloseIcon />
+            </Close>
+          </div>
+          <pre dangerouslySetInnerHTML={{ __html: css }} />
+          <div className={s.footer}>
+            <ExportButton copyText={css} />
+          </div>
         </Content>
       </Overlay>
     </Portal>
+  );
+}
+
+function CloseIcon() {
+  return (
+    <svg viewBox="0 0 16 16" width={16} height={16}>
+      <path d="M 2 2 L 14 14 M 2 14 L 14 2" stroke="black" strokeWidth={1.5} />
+    </svg>
+  );
+}
+
+function ExportButton({ copyText }: { copyText: string }) {
+  const [showMessage, setShowMessage] = useState(false);
+
+  function handleClick() {
+    if (!("navigator" in window) || showMessage) return;
+    navigator.clipboard.writeText(copyText);
+    setShowMessage(true);
+    setTimeout(() => setShowMessage(false), 2000);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      {showMessage ? "Copied to clipboard!" : "Copy CSS"}
+    </button>
   );
 }
