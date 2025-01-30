035bd0fa03f3053ed1fb400f87493d61c302247c
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2025-01-30 07:05:53 -0500

    Color mode export options

--- src/utils/generate-color-code-snippets.ts
+++ src/utils/generate-color-code-snippets.ts
@@ -1,11 +1,19 @@
 import { ColorDefinition } from "@/types";
+import { converter, formatRgb } from "culori";
+
+const rgb = converter("rgb");
 
 export function generateCSS(colors: ColorDefinition[], names: number[]) {
   const openingTag = ":where(html) {";
   const closingTag = "}";
-  const variables = colors.map(
-    (color, index) =>
-      `${space(3)}--color-primary-${names[index]}: ${color.hex};`
+  const formattedColors = colors.map((color) => {
+    const RGB = rgb(color.hex);
+    if (!RGB) throw new Error("hgg");
+    return formatRgb(RGB);
+  });
+
+  const variables = formattedColors.map(
+    (color, index) => `${space(3)}--color-primary-${names[index]}: ${color};`
   );
 
   return [openingTag, ...variables, closingTag].join("\n").trim();
