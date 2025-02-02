85bd3e6dd39811f4bcf0ac4a72790d099828208d
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2025-02-02 15:09:00 -0500

    Styling select menu

--- src/components/SelectMenu/index.tsx
+++ src/components/SelectMenu/index.tsx
@@ -21,25 +21,95 @@ function SelectMenu() {
           aria-label="Saturation Easing"
         >
           <Select.Value placeholder="Select a fruit…" />
-          <Select.Icon></Select.Icon>
+          <Select.Icon asChild>
+            <svg viewBox="0 0 16 16" fill="none" width={16} height={16}>
+              <path
+                d="M 2 5 L 8 11 L 14 5"
+                stroke="currentColor"
+                strokeWidth={1.5}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </Select.Icon>
         </Select.Trigger>
         <Select.Portal>
           <Select.Content className={s.menu}>
             <Select.Viewport>
               <Select.Item className={s.option} value="linear">
                 <Select.ItemText>Linear</Select.ItemText>
+                <Select.ItemIndicator>
+                  <svg viewBox="0 0 20 20" width={20} height={20}>
+                    <path
+                      d="M 5 10 L 9 14 L 15 6"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </Select.ItemIndicator>
               </Select.Item>
               <Select.Item className={s.option} value="easeOut">
                 <Select.ItemText>Ease Out Sine</Select.ItemText>
+                <Select.ItemIndicator>
+                  <svg viewBox="0 0 20 20" width={20} height={20}>
+                    <path
+                      d="M 5 10 L 9 14 L 15 6"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </Select.ItemIndicator>
               </Select.Item>
               <Select.Item className={s.option} value="easeOutCubic">
                 <Select.ItemText>Ease Out Cubic</Select.ItemText>
+                <Select.ItemIndicator>
+                  <svg viewBox="0 0 20 20" width={20} height={20}>
+                    <path
+                      d="M 5 10 L 9 14 L 15 6"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </Select.ItemIndicator>
               </Select.Item>
               <Select.Item className={s.option} value="easeIn">
                 <Select.ItemText>Ease In Sine</Select.ItemText>
+                <Select.ItemIndicator>
+                  <svg viewBox="0 0 20 20" width={20} height={20}>
+                    <path
+                      d="M 5 10 L 9 14 L 15 6"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </Select.ItemIndicator>
               </Select.Item>
               <Select.Item className={s.option} value="easeInCubic">
                 <Select.ItemText>Ease In Cubic</Select.ItemText>
+                <Select.ItemIndicator>
+                  <svg viewBox="0 0 20 20" width={20} height={20}>
+                    <path
+                      d="M 5 10 L 9 14 L 15 6"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </Select.ItemIndicator>
               </Select.Item>
             </Select.Viewport>
           </Select.Content>
